7f9ae4a741b038d9b37690925812ad0408cbbb88
Eonji-sw <85453429+Eonji-sw@users.noreply.github.com>
2022-04-03 04:17:56 +0900

    📰

---
+++ tsk1_mongo/package.js
@@ -0,0 +1,20 @@
+{
+    "name": "tsk1_mongo",
+    "version": "0.0.1",
+    "description": "이건 바로 몽구스",
+    "main": "app.js",
+    "scripts": {
+        "start": "nodemon app"
+    },
+    "author": "ZeroCho",
+    "license": "MIT",
+    "dependencies": {
+        "express": "^4.17.3",
+        "mongoose": "^5.13.14",
+        "morgan": "^1.10.0",
+        "nunjucks": "^3.2.3"
+    },
+    "devDependencies": {
+        "nodemon": "^2.0.15"
+    }
+}
